38ee52f1208781523511cbf8c5c06c6f23c0fd20
Asmod4n <Asmod4n@users.noreply.github.com>
2014-09-29 14:05:12 +0200

    Update sodium.rb

--- sodium.rb
+++ sodium.rb
@@ -462,7 +462,7 @@ module Sodium
       layout  :h,         [:uint64, 8],
               :t,         [:uint64, 2],
               :f,         [:uint64, 2],
-              :buf,       [:uint64, 2 * 128],
+              :buf,       [:uint8, 2 * 128],
               :buflen,    :size_t,
               :last_node, :uint8
     end
