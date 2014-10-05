3bf9e62f5152372d4ccd9d01607eb8c3a14b7c1f
Hendrik Beskow <hendrik-github2@beskow.de>
2014-10-05 12:06:54 +0200

    0.0.1

---
+++ task.rb
@@ -0,0 +1,10 @@
+﻿$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
+require 'sodium/version'
+require 'digest/sha2'
+require 'fileutils'
+
+gem_name = "ffi-libsodium-#{Sodium::VERSION}.gem"
+checksum = Digest::SHA2.new.hexdigest(File.read(gem_name))
+FileUtils.mkdir_p('checksum')
+checksum_path = "checksum/#{gem_name}.sha2"
+File.open(checksum_path, 'w' ) {|f| f.write(checksum) }
