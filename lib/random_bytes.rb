bcc47f7e14a659f00530366df25efbd0206abe05
Asmod4n <Asmod4n@users.noreply.github.com>
2014-12-11 18:07:16 +0100

    consider randombytes to never block

--- lib/random_bytes.rb
+++ lib/random_bytes.rb
@@ -5,10 +5,10 @@ module RandomBytes
   extend FFI::Library
   ffi_lib :libsodium
 
-  attach_function :randombytes_buf, [:buffer_out, :size_t], :void,  blocking: true
+  attach_function :randombytes_buf, [:buffer_out, :size_t], :void
 
-  attach_function :random,  :randombytes_random,  [],         :uint32,  blocking: true
-  attach_function :uniform, :randombytes_uniform, [:uint32],  :uint32,  blocking: true
+  attach_function :random,  :randombytes_random,  [],         :uint32
+  attach_function :uniform, :randombytes_uniform, [:uint32],  :uint32
   attach_function :close,   :randombytes_close,   [],         :int
   attach_function :stir,    :randombytes_stir,    [],         :void
 
